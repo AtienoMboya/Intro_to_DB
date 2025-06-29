3d60bfe0ce33ef6c4b5601a90fcfc8f0ccae126a
sharon <sharon@qed.ai>
2025-06-29 21:13:17 +0300

    build db

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,8 +15,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error: Unable to connect or execute command. Details: {e}")
+    except mysql.connector.Error:
+        return "Error: Unable to connect"
 
     finally:
         # Ensure the connection is closed
